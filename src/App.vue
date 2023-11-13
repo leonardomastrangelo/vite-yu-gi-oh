264abf7a52e6446d39b85aa4dd5973581c1db7e9
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-13 15:04:58 +0100

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <HeaderLM />
+  <main>
+    <div class="container">
+      <!-- header -->
+      <div>
+      </div>
+
+      <!-- main-content -->
+      <div class="row">
+        <div class="col-3">
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import HeaderLM from './components/HeaderLM.vue';
+export default {
+  name: "App",
+  components: {
+    HeaderLM,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../src/assets/css/partials/variables' as *;
+
+main {
+  background-color: $mainColor;
+}
+</style>
---
+++ src/components/HeaderLM.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="container-fluid p-3">
+        <h1 class="text-capitalize fw-light">yu-gi-oh api</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderLM"
+}
+</script>
+
+<style lang="scss" scoped></style>
